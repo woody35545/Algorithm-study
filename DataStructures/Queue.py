970dda9edadde693f0b249ac4d466ba54b7d8651
woody35545 <woody35545@gmail.com>
2022-04-09 11:18:29 +0900

    [~] Update DataStructures > Queue.py front, rear pointer를 이용하여 구현한 queue 추가

--- DataStructures/Queue.py
+++ DataStructures/Queue.py
@@ -1,11 +1,11 @@
 # Queue는 기본적으로 Rear로 들어와서(EnQueue) Front로 나가는(DeQueue) 구조임
 
 class listQueue():
-   
+
     def __init__(self):
         self.queue = []
-    
-    def enqueue(self,_data:int):
+
+    def enqueue(self, _data: int):
         self.queue.append(_data)
 
     def dequeue(self):
@@ -14,7 +14,6 @@ class listQueue():
             dequeued_object = self.queue[0]
             self.queue = self.queue[1:]
         return dequeued_object
-
 
     def peek(self):
         if not self.isEmpty():
@@ -25,23 +24,50 @@ class listQueue():
     def isEmpty(self):
         if len(self.queue) > 0:
             return True
-        
+
+        return False
+
+    def showQueue(self):
+        print(f"<REAR> ", end="")
+        for i in range(len(self.queue)):
+            print(f"[{i}] {self.queue[i]} ", end="")
+        print(f"<FRONT> ", end="\n")
+
+
+class ListQueue2():
+    def __init__(self):
+        self.queue = []
+        self.rear = 0
+        self.front = 0
+
+    def enqueue(self, _data):
+        self.queue.append(_data)
+        self.rear += 1
+
+    def dequeue(self):
+        if not self.isEmpty():
+            return_value = self.queue[self.front]
+            self.front += 1
+            return return_value
+        else:
+            return None
+
+    def size(self):
+        return self.rear - self.front
+
+    def isEmpty(self):
+        if self.size == 0:
+            return True
         return False
 
 
-    def showQueue(self):
-        print(f"<REAR> ",end = "")
-        for i in range(len(self.queue)):
-            print(f"[{i}] {self.queue[i]} ", end = "")
-        print(f"<FRONT> ",end = "\n")
-def test_queue(): 
-    
+def test_queue():
     myListQueue = listQueue()
-    
+
     myListQueue.enqueue(1)
     myListQueue.enqueue(2)
     myListQueue.enqueue(5)
     myListQueue.showQueue()
-    
+
 
 test_queue()
