9c472f16696aa975103f91cb27d3fecdc31e1a01
Woody_K <84436996+woody35545@users.noreply.github.com>
2023-01-07 16:55:01 +0900

    Update BFS.py
    add BFS with Adjacency Matrix Graph

--- DataStructures/BFS.py
+++ DataStructures/BFS.py
@@ -28,4 +28,28 @@ def bfs(graph, startVertex):
         print("")
     return visited
 
-print(bfs(test_graph,1))
+#print(bfs(test_graph,1))
+
+
+def bfs_with_adjMatrix(graph, startVertex):
+    visited, queue = [], [startVertex]
+    while queue:
+        cur_node = queue.pop(0)  # left pop
+        if cur_node not in visited:
+            visited.append(cur_node) # if current node's visited state is 'not visited', set 'visited'
+            for i in range(len(graph[cur_node])): # current node's adjacent nodes
+                if graph[cur_node][i] == 1: # check if it's connected node
+                    queue.append(i) # push current node's adjacent nodes to queue
+    return visited
+
+  
+adj_matrix = [[0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 1, 1, 0, 0, 0],
+              [0, 1, 0, 0, 1, 1, 0],
+              [0, 1, 0, 0, 1, 0, 1],
+              [0, 0, 1, 1, 0, 0, 0],
+              [0, 0, 1, 0, 0, 0, 1],
+              [0, 0, 0, 1, 0, 1, 0]]
+
+  
+print(bfs_with_adjMatrix(adj_matrix,1))
