18a992ad6cbc8f516bdd36dc70ea0b5a7a49a522
Woody_K <84436996+woody35545@users.noreply.github.com>
2023-02-03 17:39:41 +0900

    [~] S4_로프_BOJ2217.py
    Solve

--- Greedy/S4_로프_BOJ2217.py
+++ Greedy/S4_로프_BOJ2217.py
@@ -1,30 +1,21 @@
 import sys
-from collections import deque
-
+from queue import PriorityQueue
 sys_in = sys.stdin.readline
 sys_out = sys.stdout.write
 
 N = int(sys_in())
-
-lopes =[]
+lopes = PriorityQueue()
 for i in range(N):
-    lopes.append(int(sys_in()))
+    input_data = int(sys_in())
+    lopes.put(input_data)
 
-lopes.sort()
 results = []
-lopes = deque(lopes)
-#c1,c2 = min(lopes)*N, max(lopes)
-#sys_out(str(c1)) if c1 > c2 else sys_out(str(c2))
 
-while len(lopes)>0:
-    total_weight = sum(lopes)
-    min_lope = min(lopes)
-    current_max = min_lope*len(lopes)
-    #print(f"전부 사용해서 나눠들 경우 {current_max}만큼의 weight을 들 수 있습니다.")
+while lopes.qsize() > 0:
+    old_qsize = lopes.qsize()
+    min_lope = lopes.get()
+    current_max = min_lope*(old_qsize)
 
     results.append(current_max)
 
-    lopes.popleft()
-
-#print(f"최대값: {max(results)}")
 sys_out(str(max(results)))
