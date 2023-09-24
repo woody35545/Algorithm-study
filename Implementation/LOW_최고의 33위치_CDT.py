a122a3c661f88f4a35f8368abcd2c2ad2fb496b8
Woody_K <woody35545@gmail.com>
2023-09-24 20:59:47 +0900

    [+] LOW_최고의 33위치_CDT.py

---
+++ Implementation/LOW_최고의 33위치_CDT.py
@@ -0,0 +1,53 @@
+"""
+* 문제 출처: https://www.codetree.ai/missions/2/problems/best-place-of-33/description
+"""
+
+import sys
+
+# variables
+N = int(sys.stdin.readline())
+MAP = [[0]*N for _ in range(N)]
+currentMax = -1
+
+# init input
+for i in range(N):
+
+    inputLine = sys.stdin.readline()
+    numbers = inputLine.split(" ")
+    for j in range(N):
+        MAP[i][j] = int(numbers[j])
+
+def calc3by3Grid(startX,startY):
+    global MAP
+    sum = 0
+
+    for i in range(3):
+        for j in range(3):
+            sum += MAP[startY+i][startX+j]
+    return sum
+
+# Traversal
+MAX_X_MOVEMENT = N - 3 + 1
+MAX_Y_MOVEMENT = N - 3 + 1
+curX = 0
+curY = 0
+
+for d in range(MAX_Y_MOVEMENT):
+    for r in range(MAX_X_MOVEMENT):
+        # calc current max
+        currentSum = calc3by3Grid(curX,curY)
+
+        if currentSum > currentMax:
+            currentMax = currentSum
+
+        # update X position
+        curX += 1
+
+    # update Y position
+    curY += 1
+
+    # reset X, cause it's next row
+    curX = 0
+
+
+print(currentMax)
