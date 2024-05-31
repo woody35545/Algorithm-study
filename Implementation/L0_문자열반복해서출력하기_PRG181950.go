7941ef8aae67860a4e7432639b097c5d906d9674
Woody_K <woody35545@gmail.com>
2024-05-31 15:55:06 +0900

    [+] Solve L0_문자열반복해서출력하기_PRG181950 with go

---
+++ Implementation/L0_문자열반복해서출력하기_PRG181950.go
@@ -0,0 +1,20 @@
+// 문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/181950
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var inputStr string
+	var countStr string
+	
+    fmt.Scan(&inputStr, &countStr)
+
+	count, _ := strconv.Atoi(countStr)
+	
+	for i := 0; i < count; i++ {
+		fmt.Print(inputStr)
+	}
+}
